4e0480537192a0c781c10964bfac42a834177526
discoform <mh97128@gmail.com>
2017-02-28 19:42:40 -0600

    -mh combined Matt and Keisha files for starter template
    Restructured sass folders and added styles for basic elements (buttons,
    color, type, nav, etc), moved stylesheet to root (for future Wordpress
    integration), added demo.html, etc. etc. etc.

---
+++ footer.php
@@ -0,0 +1,38 @@
+<footer>
+    <div class="left">
+        contact us
+        <form action="">
+            <input type="text" placeholder="name">
+            <label for=""></label>
+
+            <input type="text" placeholder="email">
+            <label for=""></label>
+
+            <input type="text" placeholder="phone">
+            <label for=""></label>
+
+            <textarea name="" id="" placeholder="message"></textarea>
+        </form>
+    </div>
+
+    <div class="right">
+        <ul class="links">
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Show</a></li>
+            <li><a href="">Press</a></li>
+        </ul>
+
+        <ul class="social">
+            <li><a href="">Facebook</a></li>
+            <li><a href="">Twitter</a></li>
+            <li><a href="">Instagram</a></li>
+            <li><a href="">Snapchat</a></li>
+        </ul>
+
+        <p class="copyright">&copy; 2017 Boundless, MCTC Graduate Design Show</p>
+    </div>
+
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php include 'header.php'; ?>
+        <section class="boundless">
+            boundless
+        </section>
+
+        <section class="welcome">
+            <article>
+                <h1>we are boundless</h1>
+                <p>
+                    Synth messenger bag meh, art party post-ironic woke asymmetrical unicorn organic raw denim. Tousled
+                    cardigan pinterest sriracha, echo park whatever forage unicorn cronut health goth pork belly art
+                    party. Mlkshk paleo woke wolf sartorial vegan. Jean shorts shoreditch hella, meggings chia umami
+                    cold-pressed. XOXO tousled umami raw denim meh seitan listicle glossier, keytar chicharrones.
+                    Pinterest yuccie skateboard, coloring book blue bottle pabst vexillologist jean shorts mumblecore
+                    crucifix enamel pin williamsburg celiac. Ethical vinyl aesthetic, meh lo-fi deep v af drinking
+                    vinegar snackwave etsy.
+                </p>
+
+                <blockquote>
+                    <p>May 3 - 4<br>
+                        <small>Minneapolis Community & Technical College</small>
+                    </p>
+                </blockquote>
+            </article>
+        </section>
+
+        <section class="portfolio">
+
+            <article>
+                <p>view our portfolios</p>
+            </article>
+
+            <div class="web">
+                web div
+            </div>
+
+            <div class="print">
+                print div
+            </div>
+        </section>
+
+        <section class="info">
+            information
+        </section>
+
+        <section class="press">
+            press
+        </section>
+
+<?php include 'footer.php'; ?>
