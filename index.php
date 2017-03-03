5b49b0172e2ef864bb79acddcbe2cf9f18db3295
jesse <jessewollin@gmail.com>
2017-03-02 18:04:24 -0600

    added all of my working files but things seriously need work because of the angled container

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package boundless
+ */
+
+get_header(); ?>
+    <div class="behind">
+
+    </div>
+    <div class="behind">
+
+    </div>
+    <div class="behind">
+
+    </div>
+    <div class="behind">
+
+    </div>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <div class="angle">
+                <div class="angle-inner">
+                    <section class="welcome">
+                        <article class="content">
+                            <h2>we are boundless</h2>
+                            <div data-lining data-effect="slideInFromLeft" >
+                                <p>Typography (from the Greek words τύπος typos "form" and γράφειν graphein "to write") is the art and technique of arranging type to make written language most appealing to learning and recognition. The arrangement of type involves selecting typefaces, point size, line length</p>
+                                <p>line-spacing (leading), letter-spacing (tracking), and adjusting the space within letters pairs (kerning[2]). Type design is a closely related craft, sometimes considered part of typography; most typographers do not design typefaces, and some type designers do not consider themselves typographers. In modern times, typography has been put in film, television and online broadcasts to add emotion to communication.</p>
+                            </div>
+                            <blockquote>
+                                <p>May 3 - 4<br>
+                                    <small>Minneapolis Community & Technical College</small>
+                                </p>
+                            </blockquote>
+                        </article><!-- .content -->
+                    </section>
+                    <section class="portfolio">
+
+                        <article>
+                            <p>view our portfolios</p>
+                        </article>
+
+                        <div class="web">
+                            web div
+                        </div>
+
+                        <div class="print">
+                            print div
+                        </div>
+                    </section>
+
+                    <section class="info">
+                        information
+                    </section>
+
+                    <section class="press">
+                        press
+                    </section>
+                </div><!-- .angle-inner -->
+            </div><!-- .angle -->
+<?php
+get_footer();
