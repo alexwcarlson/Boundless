b8a1795bab8a54b37f8d33b0ba443d6eba70155e
keishabien <keishabien@gmail.com>
2017-03-06 22:30:16 -0600

    keisha made lots of cool changes

--- footer.php
+++ footer.php
@@ -1,38 +1,27 @@
-<footer>
-    <div class="left">
-        contact us
-        <form action="">
-            <input type="text" placeholder="name">
-            <label for=""></label>
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package boundless
+ */
 
-            <input type="text" placeholder="email">
-            <label for=""></label>
+?>
+		</main>
+	</div><!-- #content -->
 
-            <input type="text" placeholder="phone">
-            <label for=""></label>
-
-            <textarea name="" id="" placeholder="message"></textarea>
-        </form>
-    </div>
-
-    <div class="right">
-        <ul class="links">
-            <li><a href="">Portfolio</a></li>
-            <li><a href="">Show</a></li>
-            <li><a href="">Press</a></li>
-        </ul>
-
-        <ul class="social">
-            <li><a href="">Facebook</a></li>
-            <li><a href="">Twitter</a></li>
-            <li><a href="">Instagram</a></li>
-            <li><a href="">Snapchat</a></li>
-        </ul>
-
-        <p class="copyright">&copy; 2017 Boundless, MCTC Graduate Design Show</p>
-    </div>
-
-</footer>
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'boundless' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'boundless' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'boundless' ), 'boundless', '<a href="https://automattic.com/" rel="designer">Underscores.me</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,24 +1,54 @@
-<!doctype html>
-<html lang="en">
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package boundless
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>we are boundless</title>
-
-    <link rel="stylesheet" href="css/style.css">
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'boundless' ); ?></a>
+	<header id="masthead" class="site-header" role="banner">
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+            <div class="menu-toggle">
+                <div class="tri">
+                    <p>+<p>
+                </div>
+            </div>
+            <span style="font-size:30px;cursor:pointer" id="toggle">&#9776; open</span>
+            <div class="overlay" id="myNav">
+                <div class="overlay-content">
+                    <div class="name"><h2>Boundless</h2></div>
+                    <?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+                </div>
+            </div>
+		</nav><!-- #site-navigation -->
+		<div class="site-branding">
+			<?php
+			if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+			endif;
+			$description = get_bloginfo( 'description', 'display' );
+			if ( $description || is_customize_preview() ) : ?>
+				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+			<?php
+			endif; ?>
+		</div><!-- .site-branding -->
+	</header><!-- #masthead -->
 
-<!--
-make header only visible on pages such as
-portfolio and press
--->
-<header>
-
-</header>
-
-<nav>
-
-</nav>
+	<div id="content" class="site-content">
