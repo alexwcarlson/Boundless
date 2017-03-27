c359998bc5de2adf95077ae266d7e0bf5b6f9c38
Mai Xiong <smaile041297@gmail.com>
2017-03-27 12:27:35 -0500

    layout changes + added instagram feed

--- press.php
+++ press.php
@@ -1,63 +1,109 @@
-<?php //include 'header.php'; ?>
+<?php
+/*  Template Name: Press Page  */
+get_header(); ?>
 
-    <link rel="stylesheet" href="press-style.css">
 
-<h1 class="press-h1">Guest Speakers</h1>
-<section class="press-section">
-<div class="press-speakers">
-<img class="size-medium wp-image-19 press-profile" src="http://localhost:8888/boundless/wp-content/uploads/2017/03/TimBrunelle-BBDO2013-sq-lrg-300x300.jpeg" alt="" width="300" height="300"/>
-<div class="speaker-text">
-<strong>Tim Brunelle</strong><br />
-Tuesday, May 2nd<br />
-5:00 PM
-<p class="speaker-p">
-    Tim Brunelle has been at the forefront of marketing innovation and storytelling since
-1992. He's developed brand strategy, directed films, written and directed advertising
-and design as well as user experience for marketers including 3M, PwC, Thrivent,
-Skippy, Harley-Davidson, Goodyear, Porsche, Anheuser-Busch, Hormel/SPAM, Sears
-and MetLife. Tim also spent eight years helping lead Volkswagen’s “Drivers wanted”
-campaign in North America. His work has been awarded the One Show Interactive Best
-of Show, several Gold Pencils; the Grand Clio, Cannes Grand Prix, the Grand Andy,
-London International Advertising Awards Grand Prize for Websites, D&amp;AD Featured,
-AICP Finalist and inclusion in three Communication Arts Interactive Annuals. At BBDO
-MPLS, Tim acts as Creative Director for multiple Hormel brands, Andersen Windows
-and 3M, as well as oversees the agency's growing social team. Tim lives with his wife
-and three boys in Minneapolis.
-</p>
-</div>
-</div>
+    <section class="press">
+        <h1>Guest Speakers</h1>
+        <div class="content">
+        <div class="speakers">
+            <div class="profile">
+                <img class="size-medium wp-image-19"
+                     src="<?php echo get_template_directory_uri() ?>/images/8093066_300x300.jpg"
+                     alt=""/>
+            </div>
 
-<div class="press-speakers clearFix">
-<img class="alignnone size-medium wp-image-18 press-profile" src="http://localhost:8888/boundless/wp-content/uploads/2017/03/schulp-headshot-2017-300x300.jpg" alt="" width="300" height="300"/>
-<div class="speaker-text">
-<strong>Michelle Schulp</strong><br />
-Wednesday, May 3rd<br />
-5:00 PM
-
-<p class="speaker-p">
-    Michelle is an independent graphic designer and frontend developer in Minneapolis. Prior to beginning her career, she studied Visual Communications, with minors in Psychology and Sociology. As her work progressed, she also branched into front-end development and user experience design to round out her skillset. This combination of disciplines led her to adopt a strategy-based approach to design, focused on solving tangible problems and achieving real goals based on how people think.
-
-She loves the open source community, and when she is not working on projects she speaks/volunteers/organizes at events and workshops around the country. Her passions are communication and empowerment, and she believes in the power of "Why?"
-</p>
-</div>
-</div>
-</section>
-<section class="press-section">
-    <h1 class="press-h1">Connect</h1>
-    <div id="press-twitter">
-        <h3>TWITTER</h3>
-        <a class="twitter-timeline" data-lang="en" data-width="960" data-height="300" data-theme="light" data-link-color="#90d1bb" data-chrome="noheader nofooter noborders" href="https://twitter.com/mctcdesign2017">Tweets by mctcdesign2017</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-
-        <br /><br />
-
-        <a href="https://twitter.com/mctcdesign2017" class="twitter-follow-button" data-size="large" data-show-count="false">Follow @mctcdesign2017</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-    </div>
-    <div id="press-instagram">
-        <h3>INSTAGRAM</h3>
-        <div class="instagram-feed">
-<!--            <iframe src="//users.instush.com/info-slider/?cols=5&pin=false&bg=ffffff&obg=90d1bb&color=000000&user_id=2116260091&username=mxiong97&sid=-1&susername=-1&tag=-1&stype=mine&t=999999WPRV88vN8iUYkIUINk1TFhk12n6pAQmRzkOxBiWEUSQPcJAl_lzh_WExuGeGdcHCacS2okpfjTY" allowtransparency="true" frameborder="0" scrolling="no"  style="display:block;max-width:498px;width:100%;height:290px;border:none;overflow:visible; margin: 0 auto;" ></iframe>-->
+            <div class="text">
+                <h5><strong>Tim Brunelle</strong></h5>
+                <h6>Tuesday, May 2nd
+                    5:00 PM</h6>
+                <p>
+                    Tim Brunelle has been at the forefront of marketing innovation and storytelling since
+                    1992. He's developed brand strategy, directed films, written and directed advertising
+                    and design as well as user experience for marketers including 3M, PwC, Thrivent,
+                    Skippy, Harley-Davidson, Goodyear, Porsche, Anheuser-Busch, Hormel/SPAM, Sears
+                    and MetLife. Tim also spent eight years helping lead Volkswagen’s “Drivers wanted”
+                    campaign in North America. His work has been awarded the One Show Interactive Best
+                    of Show, several Gold Pencils; the Grand Clio, Cannes Grand Prix, the Grand Andy,
+                    London International Advertising Awards Grand Prize for Websites, D&amp;AD Featured,
+                    AICP Finalist and inclusion in three Communication Arts Interactive Annuals. At BBDO
+                    MPLS, Tim acts as Creative Director for multiple Hormel brands, Andersen Windows
+                    and 3M, as well as oversees the agency's growing social team. Tim lives with his wife
+                    and three boys in Minneapolis.
+                </p>
+            </div>
         </div>
-    </div>
-</section>
 
-<?php include 'footer.php'; ?>
+        <div class="speakers">
+            <div class="profile">
+                <img class="alignnone size-medium wp-image-18"
+                     src="<?php echo get_template_directory_uri() ?>/images/michelle-schulp.jpeg"
+                     alt=""/>
+            </div>
+            <div class="text">
+                <h5><strong>Michelle Schulp</strong></h5>
+                    <h6>Wednesday, May 3rd
+                        5:00 PM</h6>
+
+                    <p>
+                        Michelle is an independent graphic designer and frontend developer in Minneapolis. Prior to
+                        beginning her career, she studied Visual Communications, with minors in Psychology and Sociology. As
+                        her work progressed, she also branched into front-end development and user experience design to
+                        round out her skillset. This combination of disciplines led her to adopt a strategy-based approach
+                        to design, focused on solving tangible problems and achieving real goals based on how people think.
+
+                        She loves the open source community, and when she is not working on projects she
+                        speaks/volunteers/organizes at events and workshops around the country. Her passions are
+                        communication and empowerment, and she believes in the power of "Why?"
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="press">
+        <div class="alumni">
+            <h1>Alumni</h1>
+            <div class="content" id="panelists">
+                <h6>May 2 @ 1:00 PM</h6>
+                <ul>
+                    <li>Laurel Johnson</li>
+                    <li>Claire Campbell</li>
+                    <li>Aaron Hurst - tentative panelist</li>
+                    <li>Jerrald (Jay) Spencer</li>
+                    <li>Damien Kirchoff</li>
+                    <li>Rudy Fig (aka Sierra Riggs)</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class="press">
+        <h1>Connect</h1>
+        <div class="content">
+        <div id="press-twitter">
+            <h5>TWITTER</h5>
+            <a class="twitter-timeline" data-lang="en" data-width="960" data-height="600" data-theme="light"
+               data-link-color="#90d1bb" data-chrome="noheader nofooter noborders"
+               href="https://twitter.com/mctcdesign2017">Tweets by mctcdesign2017</a>
+            <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+            <br/><br/>
+
+            <a href="https://twitter.com/mctcdesign2017" class="twitter-follow-button" data-size="large"
+               data-show-count="false">Follow @mctcdesign2017</a>
+            <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+<!--            --><?php //echo do_shortcode("[rotatingtweets screen_name="\mxiong97\" include_rts="\1\" links_in_new_window="\1\" rotation_type="\scrollLeft\" large_follow_button="\1\" ]");
+//                echo do_shortcode('[rotatingtweets screen_name=\'mctcdesign2017\']');
+
+            ?>
+        </div>
+
+        <div id="press-instagram">
+            <h5>INSTAGRAM</h5>
+            <div class="instagram-feed">
+                <iframe src="//users.instush.com/collage/?cols=12&rows=5&sl=true&user_id=4543433049&username=mctcdesign2017&sid=-1&susername=-1&tag=-1&stype=mine&bg=transparent&space=false&rd=false&grd=false&gpd=0&drp=false&pin=false&t=999999jzQ3OnmXIBVQVfMTOzNw_4ML9gOljARFLogfY1xNiHBwYo1rNfcQqNGxUPpgHzt1sh9GfQLY0wI" allowtransparency="true" frameborder="0" scrolling="no"  style="display:block;width:1236px;height:515px;border:none;overflow:visible; margin: 0 auto;" ></iframe>
+            </div>
+            </div>
+        </div>
+    </section>
+
+<?php get_footer(); ?>
