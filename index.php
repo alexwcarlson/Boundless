378e7196eccffc49df11df36e95f23c1d4e28452
keishabien <keishabien@gmail.com>
2017-04-03 11:35:56 -0500

    keisha trying to merge with alex

--- index.php
+++ index.php
@@ -14,105 +14,99 @@
  */
 
 get_header(); ?>
+    <style>
+        .home .site-branding {
+            position: relative;
+        }
+
+        .home .site-branding:after {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: rgb(88, 89, 91);
+            z-index: -1000;
+
+        }
+    </style>
     <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
-            <section>
-                <div class="angle-behind">
+    <main id="main" class="site-main" role="main">
+    <section>
 
-                </div>
-                <div id="first" class="angle welcome">
-                    <h2 class="header-wrap">
-                        <span>we are boundless
-                    </span>
-                    </h2>
-                    <div class="angle-inner">
-                        <article class="content">
-                            <div data-lining>
-                                <p>Howdy!</p>
-                                <p>Typography (from the Greek words τύπος typos "form" and γράφειν graphein "to write")
-                                    is the art and technique of arranging type to make written language most appealing
-                                    to learning and recognition. The arrangement of type involves selecting typefaces,
-                                    point size, line length</p>
-                                <p>line-spacing (leading), letter-spacing (tracking), and adjusting the space within
-                                    letters pairs (kerning[2]). Type design is a closely related craft, sometimes
-                                    considered part of typography; most typographers do not design typefaces, and some
-                                    type designers do not consider themselves typographers. In modern times, typography
-                                    has been put in film, television and online broadcasts to add emotion to
-                                    communication.</p>
-                            </div>
-                        </article><!-- .content -->
-                    </div><!-- .angle-inner -->
-                    <blockquote>
-                        <h3 class="bottom-header">May 3 - 4<br>
-                            <small>Minneapolis Community <br>& Technical College</small>
-                        </h3>
-                    </blockquote>
-                </div><!-- .angle -->
-            </section>
+        <h2 class="header-ltr">
+            we are boundless
+        </h2>
 
-            <section class="sec-portfolio">
-                <div class="angle-behind"></div>
+        <article class="content">
+            <p class="mission">We are multidimensional designers and developers creating work
+                that embodies the breadth of our passion. Delight in the array of possibilities we
+                have to showcase.
+            </p>
 
-                <div id="second" class="angle portfolio">
-                    <div class="stripe stripe-3 stripe-first"></div>
-                    <div class="stripe stripe-2"></div>
-                    <div class="stripe stripe-1"></div>
 
-                    <div class="angle-inner">
-                        <a href="#" class="portfolio-btn">view our portfolio</a>
-                    </div><!-- .angle-inner -->
+            <h3 class="bottom-header">May 3 - 4<br>
+                Minneapolis Community <br>
+                &amp; Technical College
+            </h3>
+            <p>Where: 1501 Hennepin Ave<br>
+                Minneapolis, MN 55403<br>
+                Room L300 </p>
+            <p>Follow the signs at all main entrances of T Building and/or Whitney Hall (include map).
+                There are also directional signs at the parking entrances of every level of ramp and
+                follow the signs the whole way.
 
-                    <div class="stripe stripe-1"></div>
-                    <div class="stripe stripe-2"></div>
-                    <div class="stripe stripe-3 stripe-last"></div>
-                </div><!-- .angle -->
+            </p>
+        </article><!-- .content -->
 
-                <div class="web-full on-mobile">
-                    <a href="http://google.com">web</a>
-                </div>
-                <div class="print-full on-mobile">
-                    <a href="http://google.com">Print</a>
-                </div>
-            </section>
+    </section>
 
-            <section>
-                <div class="angle-behind">
-                    <div id="map"></div>
-                </div>
-                <div id="third" class="angle location">
-                    <h2 class="location-wrap">we are boundless</h2>
-                    <div class="angle-inner">
-                        <article class="content">
-                            <div data-lining>
-                                <p>Typography (from the Greek words τύπος typos "form" and γράφειν graphein "to write")
-                                    is the art and technique of arranging type to make written language most appealing
-                                    to learning and recognition. The arrangement of type involves selecting typefaces,
-                                    point size, line length</p>
-                                <p>line-spacing (leading), letter-spacing (tracking), and adjusting the space within
-                                    letters pairs (kerning[2]). Type design is a closely related craft, sometimes
-                                    considered part of typography; most typographers do not design typefaces, and some
-                                    type designers do not consider themselves typographers. In modern times, typography
-                                    has been put in film, television and online broadcasts to add emotion to
-                                    communication.</p>
-                            </div>
-                        </article><!-- .content -->
-                    </div><!-- .angle-inner -->
-                    <blockquote>
-                        <h3 class="bottom-header">May 3 - May 4<br>
-                            <small>Minneapolis Community <br> & Technical College</small>
-                        </h3>
-                    </blockquote>
-                </div><!-- .angle -->
-            </section>
-            <section>
-                <div class="angle-behind">
-                </div>
-                <div class="">
-                    <div class="angle-inner">
-                        <section class="press">
-                            press
-                        </section>
-                    </div><!-- .angle-inner -->
-                </div><!-- .angle -->
-            </section>
-<?php get_footer();
+    <section class="portfolio">
+
+        <p>Come delight in the work of 47 web and graphic designers at the 2017 web
+            and graphic design portfolio show, Boundless! This event allows each guest the
+            opportunity to view a wide variety of projects forged from creative passion — and
+            many late nights — and meet the artists behind the work.</p>
+        <a href="/students" class="portfolio-btn">view student work</a>
+
+
+    </section>
+
+    <section>
+
+
+        <h2 class="header-rtl">schedule</h2>
+
+        <article class="content">
+            <h4>Tuesday, May 2</h4>
+            <h5>10 AM - 8 PM</h5>
+            <ul>
+                <li>1 PM - Alumni Panel<br>
+                    <a href="">View list of alumni</a>
+                </li>
+                <li>5 PM - Keynote Speaker<br>
+                    Tim Brunelle from BBDO Minneapolis
+                </li>
+                <li>5:30 PM - Opening Reception
+                </li>
+            </ul>
+
+            <h4>Wednesday, May 3</h4>
+            <h5>10 AM - 7 PM</h5>
+            <ul>
+                <li>5 PM Keynote Speaker<br>
+                    Michelle Schulp from Marktime Media
+                </li>
+            </ul>
+        </article><!-- .content -->
+
+
+    </section>
+
+    <section class="sec-press">
+        <h2>press</h2>
+        <?php echo do_shortcode(' [instagram-feed] '); ?>
+    </section>
+
+<?php get_footer(); ?>
